2f0f78e22eac449c97887509294395508ce0c0b3
JohanComparat <johan.comparat@gmail.com>
2019-01-28 10:28:36 +0100

    python3 firefly

--- doc/one_spectra.py
+++ doc/one_spectra.py
@@ -14,28 +14,30 @@ cosmo = co.Planck13
 models_key = 'm11'
 
 def runSpec(specLiteFile):
+	t0=time.time()
 	baseN = os.path.basename(specLiteFile).split('-')
 	plate = baseN[1] 
 	mjd   = baseN[2]
 	fibre, dum = os.path.basename(baseN[3]).split('.')
-	
-	t0=time.time()
+	# This needs to be changed to your own directory
+	outputFolder = join(os.environ['EBOSSDR16_DIR'],'firefly',plate)
+	output_file = join(outputFolder , 'spFly-' + os.path.basename(specLiteFile)[5:-5] )+".fits"
+	if os.path.isfile(output_file):
+		print('already done', time.time()-t0)
+		return 0.
+	print(output_file)
+	if os.path.isdir(outputFolder)==False:
+		os.mkdir(outputFolder)
+
 	spec=gs.GalaxySpectrumFIREFLY(specLiteFile, milky_way_reddening=True)
-	spec.openObservedSDSSSpectrum(survey='sdssMain')
+	spec.openObservedSDSSSpectrum(survey='sdss4')
 
 	ageMin = 0. ; ageMax = 15.
 	ZMin = 0.001 ; ZMax = 4.
 	if spec.hdulist[2].data['CLASS'][0]=="GALAXY" and spec.hdulist[2].data['Z'][0] >  spec.hdulist[2].data['Z_ERR'][0] and spec.hdulist[2].data['Z_ERR'][0]>0 and spec.hdulist[2].data['ZWARNING'][0] ==0 :
-		
-		# This needs to be changed to your own directory
-		outputFolder = join(os.environ['FF_DIR'],'output')
-		output_file = join(outputFolder , 'spFly-' + os.path.basename(specLiteFile)[5:-5] )+".fits"
-
 		print( 'Output file:'              )
 		print( output_file                 )
 		print( "--------------------------")
-		if os.path.isdir(outputFolder)==False:
-			os.mkdir(outputFolder)
 				
 		prihdr = spm.pyfits.Header()
 		prihdr['FILE']          = os.path.basename(output_file)
@@ -56,34 +58,34 @@ def runSpec(specLiteFile):
 		did_not_converged = 0.
 
 		try :
-			model_1 = spm.StellarPopulationModel(spec, output_file, cosmo, models = models_key, model_libs = ['MILES'], imfs = ['cha'], age_limits = [ageMin,ageMax], downgrade_models = False, data_wave_medium = 'vacuum', Z_limits = [ZMin,ZMax], use_downgraded_models = True, write_results = False)
+			model_1 = spm.StellarPopulationModel(spec, output_file, cosmo, models = models_key, model_libs = ['MILES'], imfs = ['cha'], age_limits = [ageMin,ageMax], downgrade_models = False, data_wave_medium = 'vacuum', Z_limits = [ZMin,ZMax], use_downgraded_models = False, write_results = False)
 			model_1.fit_models_to_data()
 			tables.append( model_1.tbhdu )
-			# print "m1", time.time()-t0
+			print( "m1", time.time()-t0 )
 		except (ValueError):
 			tables.append( model_1.create_dummy_hdu() )
 			did_not_converged +=1
 
-		try :
-			model_1 = spm.StellarPopulationModel(spec, output_file, cosmo, models = models_key, model_libs = ['ELODIE'], imfs = ['cha'], age_limits = [ageMin,ageMax], downgrade_models = False, data_wave_medium = 'vacuum', Z_limits = [ZMin,ZMax], use_downgraded_models = True, write_results = False)
-			model_1.fit_models_to_data()
-			tables.append( model_1.tbhdu )
-			# print "m1", time.time()-t0
-		except (ValueError):
-			tables.append( model_1.create_dummy_hdu() )
-			did_not_converged +=1
+		#try :
+			#model_1 = spm.StellarPopulationModel(spec, output_file, cosmo, models = models_key, model_libs = ['ELODIE'], imfs = ['cha'], age_limits = [ageMin,ageMax], downgrade_models = False, data_wave_medium = 'vacuum', Z_limits = [ZMin,ZMax], use_downgraded_models = False, write_results = False)
+			#model_1.fit_models_to_data()
+			#tables.append( model_1.tbhdu )
+			#print( "m2", time.time()-t0 )
+		#except (ValueError):
+			#tables.append( model_1.create_dummy_hdu() )
+			#did_not_converged +=1
 
-		try :
-			model_1 = spm.StellarPopulationModel(spec, output_file, cosmo, models = models_key, model_libs = ['STELIB'], imfs = ['cha'], age_limits = [ageMin,ageMax], downgrade_models = False, data_wave_medium = 'vacuum', Z_limits = [ZMin,ZMax], use_downgraded_models = True, write_results = False)
-			model_1.fit_models_to_data()
-			tables.append( model_1.tbhdu )
-			# print "m1", time.time()-t0
-		except (ValueError):
-			tables.append( model_1.create_dummy_hdu() )
-			did_not_converged +=1
+		#try :
+			#model_1 = spm.StellarPopulationModel(spec, output_file, cosmo, models = models_key, model_libs = ['STELIB'], imfs = ['cha'], age_limits = [ageMin,ageMax], downgrade_models = False, data_wave_medium = 'vacuum', Z_limits = [ZMin,ZMax], use_downgraded_models = False, write_results = False)
+			#model_1.fit_models_to_data()
+			#tables.append( model_1.tbhdu )
+			#print( "m3", time.time()-t0)
+		#except (ValueError):
+			#tables.append( model_1.create_dummy_hdu() )
+			#did_not_converged +=1
 
 		
-		if did_not_converged < 4 :
+		if did_not_converged < 1 :
 			complete_hdus = spm.pyfits.HDUList(tables)
 			if os.path.isfile(output_file):
 				os.remove(output_file)
@@ -91,16 +93,17 @@ def runSpec(specLiteFile):
 			complete_hdus.writeto(output_file)
 	
 	print ("time used =", time.time()-t0 ,"seconds")
-	return spec
+	try:
+		return spec, model_1
+	except(TypeError):
+		return 0., 0.
+
 
 def main():
-
-	# Get argument from file
+# Get argument from file
 	file_name = sys.argv[1]
-	print file_name
-	
-
+	print( file_name)
 	# Make sure output is in correct place.
-	output_function = runSpec(file_name)
+	spec_class, model_class = runSpec(file_name)
 
 main()
---
+++ doc/run_dr16/write_scripts.py
@@ -0,0 +1,35 @@
+import sys, os
+from os.path import join
+import glob
+import numpy as n
+
+run_dir = '/home/comparat/software/linux/firefly_release/doc/run_dr16/'
+
+def writeScript(plate, spec_files):
+  f=open(run_dir + plate+".sh",'w')
+  f.write("#!/bin/bash \n")
+  f.write("#SBATCH --partition=he2srvLowP \n")
+  f.write("#SBATCH --time=2000:00:00 \n")
+  f.write("#SBATCH --nodes=1 \n")
+  f.write("#SBATCH --ntasks=1 \n")
+  f.write("#SBATCH --cpus-per-task=1 \n")
+  f.write("#SBATCH --job-name="+plate+"-ff \n")
+  f.write(" \n")
+  f.write(". /home_local/4FSOpsim/py36he2srv/bin/activate \n")
+  f.write("export OMP_NUM_THREADS=1 \n")
+  f.write(" \n")
+  f.write("cd /home/comparat/software/linux/firefly_release/doc \n")
+  f.write(" \n")
+  for spec_file in spec_files:
+    f.write("python one_spectra.py "+spec_file+" \n")
+  f.write(" \n")
+  f.close()
+
+plates = n.loadtxt('/home/comparat/software/linux/firefly_release/doc/run_dr16/plate_list', unpack=True).astype('int').astype('str')
+
+for plate in plates:
+  spec_files = n.array(glob.glob(os.path.join(os.environ['EBOSSDR16_DIR'], plate, '*.fits')))
+  spec_files.sort()
+  writeScript(plate, spec_files)
+
+
