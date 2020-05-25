b7294aa7e888299f516af1b616c0930728ec180a
Daniel Thomas <daniel.thomas@port.ac.uk>
2020-05-25 12:47:11 +0100

    File structures adjusted to be equivalent. In particular adopted use of more options from firefly_manga.py. Some minor renaming of variables. File paths in firefly_manga.py adjusted to read in example galaxy drawn from DR16 (public). Removed MPL options. Ascii file of SDSS spetrum (same as in fits file) created for firefly.py.

--- firefly_manga.py
+++ firefly_manga.py
@@ -15,47 +15,74 @@ The bin number is optional and should only be given if only a single bin is mean
 
 """
 
-import os
+import sys, os
 
+sys.path.append(os.path.join(os.getcwd(), "python"))
 os.environ["FF_DIR"] = os.getcwd()
-os.environ["PYTHONPATH"] = os.path.join(os.getcwd(), "python")
-os.environ["STELLARPOPMODELS_DIR"] = os.path.join(os.getcwd(), "stellar_population_models")
+os.environ["STELLARPOPMODELS_DIR"] = os.path.join(os.environ["FF_DIR"], "stellar_population_models")
 
-import sys
 import numpy as np
 import astropy.io.fits as fits
 import astropy.cosmology as co
+import firefly_setup as fs
+import firefly_models as fm
 import time
+
 from multiprocessing import Pool
 import multiprocessing
 
-import firefly_setup as fs
-import firefly_models as fm
-
-# ------------------------------
-# START Configuration
-
-logcube_directory = '/home/jneumann/firefly/MaNGA/MPL-9/'
-maps_directory = '/home/jneumann/firefly/MaNGA/MPL-9/'
-output_directory = '/home/jneumann/firefly/MaNGA/MPL-9/output/debug/'
-dap_path = 'home/jneumann/firefly/MaNGA/MPL-9/dapall-v2_7_1-2.4.1.fits'
-suffix = '-SPM-MaStar.fits'
-
+t0=time.time()
 cosmo = co.Planck15
-mpl = 'MPL9'                     # 'MPL5','MPL6','MPL7','MPL8','MPL9'
-models = 'm11'                     # 'bc03', 'm09', 'm11', 'MaStar'
-model_libs = ['MILES']               # 'MILES', 'STELIB', 'ELODIE', 'MARCS', for MaStar models use 'Th-MaStar' or 'E-MaStar'
-imfs = ['kr']                   # 'kr', 'ss', 'cha'; cha currently not supported for MaStar models
-age_limits = [0,'AoU']          # chose age in Gyr or 'AoU' for the age of the universe
+
+# paths to input files
+logcube_dir = 'example_data/manga/'
+maps_dir = 'example_data/manga/'
+output_dir = 'output/manga/'
+dap_file = 'example_data/manga/dapall-v2_4_3-2.2.1.fits'
+suffix = ''
+
+# define plate number, IFU number, bin number
+plate = 8080
+ifu = 12701
+bin_number = 1
+
+# masking emission lines
+# defines size of mask in pixels
+# set to value>0 for masking (20 recommended), otherwise 0
+N_angstrom_masked=0 
+# set wavelength bins to be masked
+#lines_mask = ((restframe_wavelength > 3728 - N_angstrom_masked) & (restframe_wavelength < 3728 + N_angstrom_masked)) | ((restframe_wavelength > 5007 - N_angstrom_masked) & (restframe_wavelength < 5007 + N_angstrom_masked)) | ((restframe_wavelength > 4861 - N_angstrom_masked) & (restframe_wavelength < 4861 + N_angstrom_masked)) | ((restframe_wavelength > 6564 - N_angstrom_masked) & (restframe_wavelength < 6564 + N_angstrom_masked)) 
+
+# choose model: 'm11', 'MaStar')
+model_key = 'm11'
+
+# model flavour
+# m11: 'MILES', 'STELIB', 'ELODIE', 'MARCS'
+# MaStar: 'Th-MaStar', 'E-MaStar'
+model_lib = ['MILES']
+
+# choose IMF: 'kr' (Kroupa), 'ss' (Salpeter)
+imfs = ['kr']
+
+# minimum age and metallicity of models to be used 
+# choose age in Gyr or 'AoU' for the age of the Universe
+age_limits = [0,'AoU']
 Z_limits = [-3.,3.]
-data_wave_medium = 'vacuum'     # 'vacuum', 'air'
-downgrade_models = True
-use_downgrade_models = False
-milky_way_reddening=True
-hpf_mode = 'on'                 # 'on', 'hpf_only' (i.e. E(B-V)=0)
-dust_law = 'calzetti'            # 'calzetti', 'allen', 'prevot' 
-N_angstrom_masked=20                 # Number of Angstrom masked around emission lines
+
+#specify data medium: 'air', 'vaccum'
+data_wave_medium = 'vacuum'
+#Firefly assumes flux units of erg/s/A/cm^2.
+#Choose factor in case flux is scaled
+#(e.g. flux_units=10**(-17) for MaNGA)
 flux_units=10**(-17)
+
+# set whether to correct for Milky Way reddening
+milky_way_reddening=True
+# set parameters for dust determination: 'on', 'hpf_only' (i.e. E(B-V)=0)
+hpf_mode = 'on' 
+# 'calzetti', 'allen', 'prevot' 
+dust_law = 'calzetti'
+
 
 # Only change the following parameters, if you know what you are doing.
 max_ebv = 1.5                   
@@ -70,31 +97,33 @@ pdf_sampling = 300
 def f(i):
     galaxy_bin_number  = i
     print('Fitting bin number {}'.format(i))
-    outFile = direc+'/manga-'+str(plate)+'-'+str(ifu)+'-bin'+str(i)
-    spec_MaNGA = fs.firefly_setup(maps, milky_way_reddening=milky_way_reddening, \
+    output_file = direc+'/spFly-'+str(plate)+'-'+str(ifu)+'-bin'+str(i)+'.fits'
+    spec = fs.firefly_setup(maps, milky_way_reddening=milky_way_reddening, \
                                   N_angstrom_masked=N_angstrom_masked,\
                                   hpf_mode=hpf_mode)
-    spec_MaNGA.openObservedMANGASpectrum(mpl, logcube, dap_path, galaxy_bin_number, plate, ifu)
+    spec.openMANGASpectrum(logcube, dap_file, galaxy_bin_number, plate, ifu)
     
     age_min = age_limits[0]
     if type(age_limits[1])==str:
         if age_limits[1]=='AoU':
-            age_max = cosmo.age(spec_MaNGA.redshift).value
+            age_max = cosmo.age(spec.redshift).value
         elif age_limits[1]!='AoU':
             print('Unrecognised maximum age limit. Try again.')
             sys.exit()
     else:
         age_max = age_limits[1]
     
-    model_sdss = fm.StellarPopulationModel(spec_MaNGA, outFile, cosmo, models = models, \
-                                           model_libs = model_libs, imfs = imfs, \
-                                           age_limits = [age_min,age_max], downgrade_models = downgrade_models, \
+    #prepare model templates
+    model = fm.StellarPopulationModel(spec, output_file, cosmo, models = model_key, \
+                                           model_libs = model_lib, imfs = imfs, \
+                                           age_limits = [age_min,age_max], downgrade_models = True, \
                                            data_wave_medium = data_wave_medium, Z_limits = Z_limits, \
-                                           suffix=suffix, use_downgraded_models = use_downgrade_models, \
+                                           suffix=suffix, use_downgraded_models = False, \
                                            dust_law=dust_law, max_ebv=max_ebv, num_dust_vals=num_dust_vals, \
                                            dust_smoothing_length=dust_smoothing_length,max_iterations=max_iterations, \
                                            pdf_sampling=pdf_sampling, flux_units=flux_units)
-    model_sdss.fit_models_to_data()
+    #initiate fit
+    model.fit_models_to_data()
 
 def f_mp(unique_bin_number):       #multiprocessing function
     if __name__ == '__main__':
@@ -106,20 +135,16 @@ def f_mp(unique_bin_number):       #multiprocessing function
 # ------------------------------
 # START Running firefly
 		
-start = time.time()
 print('')
 print('Starting firefly ...')
-dat = np.loadtxt(sys.argv[1])
-plate = int(dat[0])
-ifu = int(dat[1])
 print('Plate = {}, IFU = {}'.format(plate,ifu))
 
 # Set MAPS and LOGCUBE paths.
-logcube = os.path.join(logcube_directory,'manga-'+str(plate)+'-'+str(ifu)+'-LOGCUBE-VOR10-MILESHC-MASTARHC.fits.gz')
-maps = os.path.join(maps_directory,'manga-'+str(plate)+'-'+str(ifu)+'-MAPS-VOR10-MILESHC-MASTARHC.fits.gz')
+logcube = os.path.join(logcube_dir,'manga-'+str(plate)+'-'+str(ifu)+'-LOGCUBE-VOR10-GAU-MILESHC.fits.gz')
+maps = os.path.join(maps_dir,'manga-'+str(plate)+'-'+str(ifu)+'-MAPS-VOR10-GAU-MILESHC.fits.gz')
 
 # Create output path if it doesn't exist.
-direc = os.path.join(output_directory,str(plate),str(ifu))
+direc = os.path.join(output_dir,str(plate),str(ifu))
 if not os.path.exists(direc):
     os.makedirs(direc)
 
@@ -128,10 +153,6 @@ maps_header = fits.open(maps)
 unique_bin_number = list(np.unique(maps_header['BINID'].data)[1:])
 print('Number of bins = {}'.format(len(unique_bin_number)))
 
-if len(dat)==3:
-	bin_number = int(dat[2])
-	f(bin_number)
-else:
-	N = f_mp(unique_bin_number)
+f(bin_number)
 
-print('Time to complete: ', (time.time())-start)
+print('Time to complete: ', (time.time())-t0)
